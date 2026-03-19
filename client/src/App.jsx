56e852b676de808470fce337fe5a4faf8424f1b9
rajtrisha11 <rajtrisha1103@gmail.com>
2026-03-19 10:52:53 +0530

    Added redux toolkit dependency

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,25 @@
+import { Routes, Route } from "react-router-dom"
+import Layout from "./pages/Layout"
+import Login from "./pages/Login"
+import Home from "./pages/Home"
+import Dashboard from "./pages/Dashboard"
+import ResumeBuilder from "./pages/ResumeBuilder"
+import Preview from "./pages/Preview"
+
 function App() {
   return (
-    <div>
-      <h1>Resume Builder</h1>
-      <p>Your app is running 🚀</p>
-    </div>
+    <Routes>
+      {/* Public Route */}
+      <Route path="/" element={<Login />} />
+
+      {/* Layout Routes */}
+      <Route path="/" element={<Layout />}>
+        <Route path="home" element={<Home />} />
+        <Route path="dashboard" element={<Dashboard />} />
+        <Route path="builder" element={<ResumeBuilder />} />
+        <Route path="preview" element={<Preview />} />
+      </Route>
+    </Routes>
   )
 }
 
