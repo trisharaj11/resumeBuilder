7b3a259decd435d315b7e6942484956bc3cad7ab
rajtrisha11 <rajtrisha1103@gmail.com>
2026-03-19 11:04:08 +0530

    final routing

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,23 +1,30 @@
 import { Routes, Route } from "react-router-dom"
 import Layout from "./pages/Layout"
 import Login from "./pages/Login"
-import Home from "./pages/Home"
 import Dashboard from "./pages/Dashboard"
+import Home from "./pages/Home"
 import ResumeBuilder from "./pages/ResumeBuilder"
 import Preview from "./pages/Preview"
 
 function App() {
   return (
     <Routes>
-      {/* Public Route */}
       <Route path="/" element={<Login />} />
 
-      {/* Layout Routes */}
-      <Route path="/" element={<Layout />}>
-        <Route path="home" element={<Home />} />
-        <Route path="dashboard" element={<Dashboard />} />
-        <Route path="builder" element={<ResumeBuilder />} />
-        <Route path="preview" element={<Preview />} />
+      <Route path="/dashboard" element={<Layout />}>
+        <Route index element={<Dashboard />} />
+      </Route>
+
+      <Route path="/home" element={<Layout />}>
+        <Route index element={<Home />} />
+      </Route>
+
+      <Route path="/builder" element={<Layout />}>
+        <Route index element={<ResumeBuilder />} />
+      </Route>
+
+      <Route path="/preview" element={<Layout />}>
+        <Route index element={<Preview />} />
       </Route>
     </Routes>
   )
